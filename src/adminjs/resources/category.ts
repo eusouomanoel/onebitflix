d0b3d40c69113bc413f371186c022c4f2658aa54
Manoel Neto <manoeln@outlook.com>
2023-01-28 13:36:51 -0300

    Tabela de usuários

--- src/adminjs/resources/category.ts
+++ src/adminjs/resources/category.ts
@@ -4,6 +4,6 @@ export const categoryResourceOptions: ResourceOptions = {
     navigation: 'Catálogo',
     editProperties: ['name', 'position'],
     filterProperties: ['name', 'position', 'createdAt', 'updatedAt'],
-    listProperties: ['ind', 'name', 'position'],
+    listProperties: ['id', 'name', 'position'],
     showProperties: ['id', 'name', 'position', 'createdAt', 'updatedAt']
 }
