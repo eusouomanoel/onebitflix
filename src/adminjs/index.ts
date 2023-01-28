e8461c4dd4cc20009ace1615a7ff86972512e257
Manoel Neto <manoeln@outlook.com>
2023-01-28 15:08:27 -0300

    Personalizando o Dashboard

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -3,7 +3,7 @@ import AdminJSExpress from "@adminjs/express"
 import AdminJSSequelize from "@adminjs/sequelize"
 import { sequelize } from "../database";
 import { adminJsResources } from "./resources";
-import { User } from "../models";
+import { Category, Course, Episode, User } from "../models";
 import bcrypt from 'bcrypt'
 import { locale } from "./locale";
 
@@ -14,6 +14,22 @@ export const adminJs = new AdminJS({
     rootPath: "/admin", //definir a rota pra o painel de administração
     resources: adminJsResources,
     locale: locale,
+    dashboard: {
+        component: AdminJS.bundle("./components/Dashboard"),
+        handler: async (req, res, context) => {
+            const courses = await Course.count()
+            const episodes = await Episode.count()
+            const categories = await Category.count()
+            const standardUsers = await User.count({ where: { role: 'user' } })
+
+            res.json({
+                'Cursos': courses,
+                'Episódios': episodes,
+                'Categorias': categories,
+                'Usuários': standardUsers
+            })
+        }
+    },
     branding: {
         companyName: 'OneBitFlix',
         logo: '/onebitflix.svg',
@@ -54,5 +70,5 @@ export const adminJsRouter = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
     cookiePassword: 'senha-de-cookie'
 }, null, {
     resave: false,
-    saveUninitialied: false
+    saveUninitialized: false
 })
