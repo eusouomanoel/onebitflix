24f0960248dd25bb57d36a9d673848dcdc968c9d
Manoel Neto <manoeln@outlook.com>
2023-01-28 10:51:08 -0300

    Upload de arquivos no AdminJS

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,11 +1,16 @@
 import { Category } from "./Category";
 import { Course } from "./Course";
+import { Episode } from "./Episode";
 
 //Relacionamento entre as categorias e cursos
 Category.hasMany(Course)
 Course.belongsTo(Category)
+Course.hasMany(Episode)
+Episode.belongsTo(Course)
+
 
 export {
     Category,
-    Course
+    Course,
+    Episode
 }
