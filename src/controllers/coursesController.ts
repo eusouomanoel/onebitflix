97a4439a18acb0d21ecf7298617eb6df4d6c9bd7
Manoel Neto <manoeln@outlook.com>
2023-01-30 19:37:54 -0300

    Buscando por curso especifico

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { getPaginationParams } from "../helpers/getPaginationParams";
 import { courseService } from "../services/courseService";
 
 export const coursesController = {
@@ -26,7 +27,20 @@ export const coursesController = {
             }
         }
     },
-
+    //GET /courses/search?name=
+    search: async (req: Request, res: Response) => {
+        const { name } = req.query
+        const [page, perPage] = getPaginationParams(req.query)
+        try {
+            if (typeof name !== 'string') throw new Error('name param must be of type strig')
+            const courses = await courseService.findByName(name, page, perPage)
+            return res.json(courses)
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({ message: error.message })
+            }
+        }
+    },
     //GET /courses/:id
     show: async (req: Request, res: Response) => {
         const { id } = req.params
