861d24923506574539bdeb6ca760298d7011c14f
Manoel Neto <manoeln@outlook.com>
2023-01-31 10:30:43 -0300

    Login com JSON Web Token

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
+import { jwtService } from "../services/jwtService";
 import { userService } from "../services/userService";
 
 export const authController = {
+    //POST /auth/register
     register: async (req: Request, res: Response) => {
         const { firstName, lastName, phone, birth, email, password } = req.body
 
@@ -25,6 +27,42 @@ export const authController = {
         } catch (error) {
             if (error instanceof Error) {
                 return res.status(400).json({ message: error.message })
+            }
+        }
+    },
+    // POST /auth/login
+    login: async (req: Request, res: Response) => {
+        const { email, password } = req.body
+
+        try {
+            const user = await userService.findByEmail(email)
+
+            if (!user) {
+                return res.status(401).json({ message: 'E-mail não registrado' })
+            }
+
+            user.checkPassword(password, (err, isSame) => {
+                if (err) {
+                    return res.status(400).json({ message: err.message })
+                }
+
+                if (!isSame) {
+                    return res.status(401).json({ message: 'Senha incorreta' })
+                }
+
+                const payload = {
+                    id: user.id,
+                    firstName: user.firstName,
+                    email: user.email
+                }
+
+                const token = jwtService.signToken(payload, '7d')
+
+                return res.json({ authenticated: true, ...payload, token })
+            })
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
             }
         }
     }
